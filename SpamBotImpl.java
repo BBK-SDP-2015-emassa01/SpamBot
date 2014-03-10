9a83007cbac25dc7b08205504da87a096fd84d07
Esha Massand <ubemas001@mail.bbk.ac.uk>
2014-03-10 20:25:53 +0000

    changes

--- SpamBotImpl.java
+++ SpamBotImpl.java
@@ -1,3 +1,5 @@
+package SpamBot;
+
 
 import ConcurrencyBookExercise.CallableDemo;
 
--- WebPageImpl.java
+++ WebPageImpl.java
@@ -1,4 +1,5 @@
 
+import SpamBot.SpamBotImpl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
